bf21015896c3808b55bad3219bb5c4fa8ed8220e
Plengpai Ratnajarn <pinplengpai@gmail.com>
2019-12-07 12:17:22 -0500

    add files and functions folder

---
+++ functions/customs-functions/cf.js
@@ -0,0 +1,7 @@
+//function definition
+function calculateBill(){
+  //this is the function body
+  console.log('running calculation Bill!!');
+}
+//function call ** run
+calculateBill();
---
+++ functions/types.js
@@ -0,0 +1,4 @@
+let isDrawing = false;
+let age = 18;
+const ofAge = age > 19;
+console.log(ofAge);

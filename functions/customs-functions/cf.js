517de70ea97e1d2b038a85006dde860e46866210
Plengpai Ratnajarn <pinplengpai@gmail.com>
2019-12-07 15:02:24 -0500

    learn about customs-fuctions

--- functions/customs-functions/cf.js
+++ functions/customs-functions/cf.js
@@ -1,7 +1,33 @@
 //function definition
-function calculateBill(){
+function calculateBill(billAmount, taxRate = 5){
   //this is the function body
   console.log('running calculation Bill!!');
+  const total = billAmount * (1 + taxRate); // temporary variable
+  return total;
 }
+
+console.log(calculateBill(100))
 //function call ** run
-calculateBill();
+const wesTotal = 500
+const wesTaxRate = 0.3;
+// const myTotal = calculateBill(wesTotal, wesTaxRate);
+// console.log (myTotal);
+
+// function definition
+function sayHiTo(firstName) {
+  return `Hello ${firstName}`;
+}
+//const greeting = sayHiTo('Bennn');
+
+//console.log(greeting);
+
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+
+function yell(name) {
+  return `HEY ${name.toUpperCase()}`;
+}
+
+
+

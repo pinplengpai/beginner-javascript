a2b3320d4b4d1614b5bf6dbe50d4516fc20b6858
Plengpai Ratnajarn <pinplengpai@gmail.com>
2019-12-07 17:55:22 -0500

    learnt different way to declare functions

--- functions/customs-functions/way-to-make-functions.js
+++ functions/customs-functions/way-to-make-functions.js
@@ -1,1 +1,21 @@
-console.log(it works!);
+//first class citizen
+// const age = 100;
+// const cool = true;
+// these are values that are numbers, objects, strings and boolean.
+// function can be passed to other function, function can be stored and can do many things.
+
+// Different way of functions.
+//normal function
+// function docterize(firstName) {
+//   return `Dr ${firstName}`;
+// };
+
+// anonymous functions
+// function (firstName) {
+//   return `Dr. ${firstName}`;
+// };
+
+// function expression (is where you store a function as a value in a variable)
+const docterize = function(firstName) {
+  return `Dr. ${firstName}`;
+};
